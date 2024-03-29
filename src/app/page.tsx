de0b0c76ce95b65382ddf80f34651b8164c64449
ilia-io <likeapu@gmail.com>
2024-03-29 15:40:00 +0300

    add subcategories filter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,6 +12,7 @@ import axios from 'axios';
 import { ChevronDown, Filter } from 'lucide-react';
 import { useState } from 'react';
 import Product, * as ProductComponent from '@/components/Products/Product';
+import ProductSkeleton from '@/components/Products/ProductSkeleton';
 
 const SORT_OPTIONS = [
   {
@@ -27,6 +28,13 @@ const SORT_OPTIONS = [
     value: 'price-desc',
   },
 ] as const;
+
+const SUBCATEGORIES = [
+  { name: 'T-Shirts', selected: true, href: '#' },
+  { name: 'Hoodies', selected: false, href: '#' },
+  { name: 'Sweatshirts', selected: false, href: '#' },
+  { name: 'Accessories', selected: false, href: '#' },
+];
 
 export default function Home() {
   const [filter, setFilter] = useState({ sort: 'none' });
@@ -50,51 +58,65 @@ export default function Home() {
 
   console.log(products);
 
-console.log('process.env', process.env.NEXT_PUBLIC_SOME_VAR);
-return (
-  <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-    <div className="flex items-baseline justify-between border-b border-gray-200 pb-6 pt-24">
-      <h1 className="text-4xl font-bold tracking-tight text-gray-900">
-        High-quality cotton selection {process.env.NEXT_PUBLIC_SOME_VAR}
-      </h1>
-      <div className="flex items-center">
-        <DropdownMenu>
-          <DropdownMenuTrigger className="group inline-flex justify-center text-sm font-medium text-gray-700 hover:text-gray-900">
-            Sort
-            <ChevronDown className="-mr-1 ml-1 h-5 w-5 flex-shrink-0 text-gray-400 group-hover:text-gray-500" />
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            {SORT_OPTIONS.map((option) => (
-              <button
-                className={cn('text-left w-full block px-4 py-2 text-sm', {
-                  'text-gray-900 bg-gray-100': option.value === filter.sort,
-                  'text-gray-500': option.value !== filter.sort,
-                })}
-                onClick={() => handleSort(option.value)}
-                key={option.name}
-              >
-                {option.name}
-              </button>
-            ))}
-          </DropdownMenuContent>
-        </DropdownMenu>
-        <button className="-m-2 ml-4 p-2 text-gray-400 hover:text-gray-500 sm:ml-6 lg:hidden">
-          <Filter className="h-5 w-5" />
-        </button>
+  return (
+    <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+      <div className="flex items-baseline justify-between border-b border-gray-200 pb-6 pt-24">
+        <h1 className="text-4xl font-bold tracking-tight text-gray-900">
+          High-quality cotton selection
+        </h1>
+        <div className="flex items-center">
+          <DropdownMenu>
+            <DropdownMenuTrigger className="group inline-flex justify-center text-sm font-medium text-gray-700 hover:text-gray-900">
+              Sort
+              <ChevronDown className="-mr-1 ml-1 h-5 w-5 flex-shrink-0 text-gray-400 group-hover:text-gray-500" />
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              {SORT_OPTIONS.map((option) => (
+                <button
+                  className={cn('text-left w-full block px-4 py-2 text-sm', {
+                    'text-gray-900 bg-gray-100': option.value === filter.sort,
+                    'text-gray-500': option.value !== filter.sort,
+                  })}
+                  onClick={() => handleSort(option.value)}
+                  key={option.name}
+                >
+                  {option.name}
+                </button>
+              ))}
+            </DropdownMenuContent>
+          </DropdownMenu>
+          <button className="-m-2 ml-4 p-2 text-gray-400 hover:text-gray-500 sm:ml-6 lg:hidden">
+            <Filter className="h-5 w-5" />
+          </button>
+        </div>
       </div>
-    </div>
-    <section className="pb-24 pt-6 ">
-      <div className="grid grid-cols-1 gap-x-8 gap-y-10  lg:grid-cols-4">
-        {/* Filters */}
-        <div></div>
-        {/* Product grid */}
-        <ul className="lg:col-span-3 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
-          {products?.map((product) => (
-            <Product key={product.id} product={product.metadata!} />
-          ))}
-        </ul>
-      </div>
-    </section>
-  </main>
-);
+      <section className="pb-24 pt-6 ">
+        <div className="grid grid-cols-1 gap-x-8 gap-y-10  lg:grid-cols-4">
+          {/* Filters */}
+          <div className="hidden lg:block">
+            <ul className="space-y-4 border-b border-gray-200 pb-6 test-sm font-medium text-gray-900">
+              {SUBCATEGORIES.map((category) => (
+                <li key={category.name}>
+                  <button
+                    disabled={!category.selected}
+                    className="disabled:cursor-not-allowed disabled:opacity-60"
+                  >
+                    {category.name}
+                  </button>
+                </li>
+              ))}
+            </ul>
+          </div>
+          {/* Product grid */}
+          <ul className="lg:col-span-3 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+            {products
+              ? products.map((product) => (
+                  <Product key={product.id} product={product.metadata!} />
+                ))
+              : new Array(12).fill(null).map((_, i) => <ProductSkeleton key={i} />)}
+          </ul>
+        </div>
+      </section>
+    </main>
+  );
 }
